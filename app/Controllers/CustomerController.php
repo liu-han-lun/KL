35bc7d567bbb405a1eb6b3e5d2f69c1c79606440
liu-han-lun <nihgfwc@gmail.com>
2020-07-17 17:04:43 +0800

    text 5

---
+++ app/Controllers/CustomerController.php
@@ -0,0 +1,27 @@
+<?php namespace App\Controllers;
+
+	use App\Models\CustomerModel ;
+	/**
+	 * 
+	 */
+	class CustomerController  
+	{
+		
+		public function __construct()
+		{
+			$this->model = new CustomerModel() ;
+
+		}
+
+		
+
+
+
+		
+	}
+
+
+
+
+
+ ?>
---
+++ app/Models/CustomerModel.php
@@ -0,0 +1,65 @@
+<?php  namespace App\Models ;
+
+
+	/**
+	  * 
+	  */
+	 class CustomerModel extends \CodeIgniter\Model
+	 {
+	 	
+	 	public function __construct()
+	 	{
+	 		$this->db = \Config\Database::connect() ;
+	 	}
+
+
+
+	 	public function getList()
+	 	{
+
+
+	 		$builder = $this->db->table('customer-info') ;
+	 		$query = $builder->orderBy('id')->get() ;
+	 		return $query->getResult() ; 
+
+	 	}
+
+
+	 	public function addList( $CustomerList = NULL )
+	 	{
+
+	 		$builder = $this->db->table('customer-info') ;
+	 		$query = $builder->insert($CustomerList) ;
+
+	 	}
+
+
+	 	public function deleteList($id = NULL)
+	 	{
+
+	 		$builder = $this->db->table('customer-info') ;
+	 		$query = $builder->delete( [ 'id' => $id ] ) ;
+	 	}
+
+
+	 	public function editList( $id = NULL , $edit = NULL )
+	 	{
+
+	 		$builder = $this->db->table('customer-info') ;
+	 		$builder->where( 'id' , $id ) ;
+	 		$builder->update( $edit ) ;
+
+
+	 	}
+
+
+
+
+	 } 
+
+
+
+
+
+
+ ?>
